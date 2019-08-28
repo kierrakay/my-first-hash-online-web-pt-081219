644412972d5ba1f78567468ea2c81a0ae80174f8
Kierra M <k.merriman32@hotmail.com>
2019-08-28 21:36:54 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,7 +38,6 @@ shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
  shipping_manifest["gun powder"] = 4
-shipping_manifest
 
 
   # return the shipping_manifest hash below
