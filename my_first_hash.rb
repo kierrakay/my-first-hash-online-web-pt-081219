5798a078642af93b09f58c032bf0c3ace0b0fad0
Kierra M <k.merriman32@hotmail.com>
2019-08-28 21:36:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,7 +37,7 @@ def adding
 shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
- ["gun powder"] = 4
+ shipping_manifest["gun powder"] = 4
 shipping_manifest
 
 
